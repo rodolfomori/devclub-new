8bebbf86641b450450a27212dcdc873b4ea92b61
Rodolfo Mori <rodolfomori@Rodolfos-MacBook-Pro.local>
2023-11-22 15:51:23 -0300

    change onsignal notifications

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -8,14 +8,12 @@ import db from '../../services/firebase'
 import FullHome from '../FullHome'
 import BasicHome from '../BasicHome'
 import { useKeepAwake } from 'expo-keep-awake'
-// import { Platform } from 'react-native'
 
 function App() {
   useKeepAwake()
   const [basicMode, setBasicMode] = useState(false)
 
   async function checkUpdates() {
-    // let androidON = false
     try {
       const colRef = collection(db, 'toggle_list')
 
@@ -25,21 +23,6 @@ function App() {
         ...doc.data(),
         id: doc.id,
       }))[0].basicMode
-
-      // const onlyIos = snapshots.docs.map((doc) => ({
-      //   ...doc.data(),
-      //   id: doc.id,
-      // }))[0].onlyIos
-
-      // if (Platform.OS === 'ios') {
-      //   androidON = true
-      // } else {
-      //   if (onlyIos) {
-      //     androidON = false
-      //   } else {
-      //     androidON = true
-      //   }
-      // }
 
       setBasicMode(docs)
     } catch (err) {
@@ -52,36 +35,10 @@ function App() {
     checkUpdates()
   }, [])
 
+  //notifications
   OneSignal.Debug.setLogLevel(LogLevel.Verbose)
   OneSignal.initialize(Constants.expoConfig.extra.oneSignalAppId)
-
-  // Also need enable notifications to complete OneSignal setup
   OneSignal.Notifications.requestPermission(true)
-
-  // OneSignal.setAppId('1ee336fa-54a8-4c77-b6c5-8c44a658691b')
-
-  // OneSignal.promptForPushNotificationsWithUserResponse()
-
-  // // Method for handling notifications received while app in foreground
-  // OneSignal.setNotificationWillShowInForegroundHandler(
-  //   (notificationReceivedEvent) => {
-  //     console.log(
-  //       'OneSignal: notification will show in foreground:',
-  //       notificationReceivedEvent,
-  //     )
-  //     const notification = notificationReceivedEvent.getNotification()
-  //     console.log('notification: ', notification)
-  //     const data = notification.additionalData
-  //     console.log('additionalData: ', data)
-  //     // Complete with null means don't show a notification.
-  //     notificationReceivedEvent.complete(notification)
-  //   },
-  // )
-
-  // // Method for handling notifications opened
-  // OneSignal.setNotificationOpenedHandler((notification) => {
-  //   console.log('OneSignal: notification opened:', notification)
-  // })
 
   return basicMode ? <BasicHome /> : <FullHome />
 }
